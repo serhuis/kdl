280f9a8043e01b0ed8d8e7b967c370b2de517729
serhiy-pc\serhiy <serhuis.s@gmail.com>
2017-09-26 10:59:14 +0300

    changed MCU to STM32L152

---
+++ Objects/system_stm32l1xx.d
@@ -0,0 +1,8 @@
+.\objects\system_stm32l1xx.o: RTE\Device\STM32L152RB\system_stm32l1xx.c
+.\objects\system_stm32l1xx.o: d:\Keil_v5\ARM\PACK\Keil\STM32L1xx_DFP\1.0.2\Device\Include\stm32l1xx.h
+.\objects\system_stm32l1xx.o: d:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cm3.h
+.\objects\system_stm32l1xx.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\system_stm32l1xx.o: d:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cminstr.h
+.\objects\system_stm32l1xx.o: d:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\cmsis_armcc.h
+.\objects\system_stm32l1xx.o: d:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cmfunc.h
+.\objects\system_stm32l1xx.o: d:\Keil_v5\ARM\PACK\Keil\STM32L1xx_DFP\1.0.2\Device\Include\system_stm32l1xx.h
--- RTE/RTE_Components.h
+++ RTE/RTE_Components.h
@@ -14,16 +14,15 @@
 /*
  * Define the Device Header File: 
  */
-#define CMSIS_device_header "stm32f10x.h"
+#define CMSIS_device_header "STM32L1xx.h"
 
 #define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
         #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
-#define RTE_DEVICE_STDPERIPH_DMA
-#define RTE_DEVICE_STDPERIPH_FRAMEWORK
-#define RTE_DEVICE_STDPERIPH_GPIO
-#define RTE_DEVICE_STDPERIPH_I2C
-#define RTE_DEVICE_STDPERIPH_RCC
-#define RTE_DEVICE_STDPERIPH_TIM
-#define RTE_DEVICE_STDPERIPH_USART
+#define RTE_Compiler_IO_STDERR          /* Compiler I/O: STDERR */
+        #define RTE_Compiler_IO_STDERR_User     /* Compiler I/O: STDERR User */
+#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
+        #define RTE_Compiler_IO_STDIN_User      /* Compiler I/O: STDIN User */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+        #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
 
 #endif /* RTE_COMPONENTS_H */
